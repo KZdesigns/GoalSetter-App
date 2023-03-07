6f2a308134354378837dbbaf2ff2ee5f607b0af5
Kyle Zimmerman <kzdesignsios@gmail.com>
2023-03-06 21:51:52 -0800

    basic layout & functionality complete

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setGoalText] = useState("");
+  const [goalList, setGoalList] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    setGoalList((prevState) => [...prevState, enteredGoalText]);
+    setGoalText("");
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.AppContainer}>
+      <View style={styles.InputContainer}>
+        <TextInput
+          style={styles.Input}
+          placeholder="Type goal here"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        ></TextInput>
+        <Button title="Add Goal" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.GoalsContainer}>
+        {goalList.map((goal, index) => (
+          <Text key={index}>{goal}</Text>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  AppContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  InputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+  },
+  Input: {
+    borderWidth: 1,
+    borderColor: "black",
+    padding: 5,
+    width: "70%",
+  },
+  GoalsContainer: {
+    flex: 6,
   },
 });
